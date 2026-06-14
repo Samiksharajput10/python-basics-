eb1f02c7656fd96716500ce1c74e0b783de8c571
Samiksharajput10 <rupeshkumarsingh1026@gmail.com>
2026-06-14 15:22:54 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,15 @@
+a = int(input())
+b = int(input())
+
+sum = (a+b)
+print("Addition:",sum)
+
+difference = (a-b)
+print("Difference:",difference)
+
+product = (a*b)
+print("Product:",product)
+
+division = (a/b)
+print("Division:",division)
+
